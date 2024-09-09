4fe35886c9b28058febc0de3131cdc79ce43e247
Matias Arola <matias.arola@ellie.ai>
2024-09-09 10:14:41 +0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Popover } from "@mui/material";
+import { MouseEvent, useCallback, useState } from "react";
+import ReactFlow from "reactflow";
+ 
+import 'reactflow/dist/style.css';
+import './App.css'
 
-function App() {
+
+const initialNodes = [
+  { id: '1', position: { x: 0, y: 0 }, data: { label: '1' }, type: 'node' },
+  { id: '2', position: { x: 0, y: 100 }, data: { label: '2' }, type: 'node'  },
+];
+const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
+
+const Node = (props: any) => {
+  const [popover, setPopover] = useState<any>(null)
+
+  const onContextMenu = useCallback((e: MouseEvent) => {
+    e.preventDefault()
+    setPopover({left: e.clientX, top: e.clientY})
+  }, [])
+
+  const onClosePopover = useCallback(() => setPopover(null), [])
+
+  return <>
+    <div 
+      className="react-flow__node-default" 
+      onContextMenu={onContextMenu}>
+      <p>{props.data.label}</p>
+    </div>
+    
+    <Popover 
+      open={!!popover} 
+      onClose={onClosePopover}
+      anchorReference="anchorPosition" 
+      anchorPosition={popover}>
+      Hello world
+    </Popover>
+  </>
+}
+
+const nodeTypes = {node: Node}
+ 
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <ReactFlow nodes={initialNodes} edges={initialEdges} nodeTypes={nodeTypes} />
     </div>
   );
 }
-
-export default App;
